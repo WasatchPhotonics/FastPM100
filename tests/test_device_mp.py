23214a532accfe2e4ca44823e5d4cf45dacb8061
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-12-10 16:06:46 -0500

    through disconnect of worker object on close

---
+++ tests/test_device_mp.py
@@ -0,0 +1,36 @@
+""" Testing tests for getting log prints in multiprocessing objects
+with py.test and pyside, etc.
+"""
+
+import sys
+import time
+
+import logging
+log = logging.getLogger()
+strm = logging.StreamHandler(sys.stderr)
+frmt = logging.Formatter("%(name)s - %(levelname)s %(message)s")
+strm.setFormatter(frmt)
+log.addHandler(strm)
+log.setLevel(logging.DEBUG)
+
+
+class TestProcessLogs:
+    def test_object_has_log_debug(self):
+        from fastpm100 import devices
+        mp_device = devices.QueueMPDevice()
+        mp_device.create()
+
+        sleep_dur = 1
+        count = 0
+        while(1):
+            log.debug("Sleep: %s", sleep_dur)
+            time.sleep(sleep_dur)
+            count += 1
+            if count > 3:
+                break
+
+        mp_device.close()
+
+
+    #def test_object_called_linked_in_pyside_has_log_prints(self):
+

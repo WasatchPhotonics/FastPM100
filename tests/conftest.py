1e48c37411cf8fac8e422827369f7430116a0783
Nathan Harrington <nharrington@wasatchphotonics.com>
2016-01-12 07:52:59 -0500

    control mode in simulation

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+""" From: https://pytest.org/latest/example/simple.html, add a command
+line switch to run the tests that require power meter hardware. This is
+to isolate any log file processing issues.
+"""
+
+import pytest
+def pytest_addoption(parser):
+    parser.addoption("--hardware", action="store_true",
+        help="run tests requiring physical hardware")
